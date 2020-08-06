c75f7d595651caa680bf368ee3bfdbda4590c403
Azad Sadigli <azad@sadiq.li>
2020-08-07 00:57:57 +0400

    @

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 
-const port = 3000;
+const port = 3002;
 const path = require('path');
 const flash = require('connect-flash');
 const router = require('./src/router');
